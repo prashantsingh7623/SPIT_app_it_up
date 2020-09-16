c938697108667e7faef81265310874ed96cfbb0e
prashantsingh <prashantsingh7623@gmail.com>
2020-09-16 20:49:37 +0530

    registered app to firebase

---
+++ app/src/main/java/adbudh/spit/admin/AdminLandingActivity.java
@@ -0,0 +1,16 @@
+package adbudh.spit.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import adbudh.spit.R;
+
+public class AdminLandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_landing);
+    }
+}

01ef76a2d5c646e06d75d4f2bf9554c34193e0d6
prashantsingh <prashantsingh7623@gmail.com>
2020-09-17 00:11:34 +0530

    login layout design complete

--- app/src/main/java/adbudh/spit/admin/LoginActivity.java
+++ app/src/main/java/adbudh/spit/admin/LoginActivity.java
@@ -1,14 +1,16 @@
-package adbudh.spit;
+package adbudh.spit.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import adbudh.spit.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }

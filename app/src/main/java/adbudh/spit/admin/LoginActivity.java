181689f9d481c8f623bb9ccda7113630cbcb3a40
prashantsingh <prashantsingh7623@gmail.com>
2020-09-17 15:47:02 +0530

    login functionality completed

--- app/src/main/java/adbudh/spit/admin/LoginActivity.java
+++ app/src/main/java/adbudh/spit/admin/LoginActivity.java
@@ -1,16 +1,86 @@
 package adbudh.spit.admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 import adbudh.spit.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private String email, password;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        loginButton = findViewById(R.id.button_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextInputLayout input_email = findViewById(R.id.input_email);
+                TextInputLayout input_password = findViewById(R.id.input_password);
+                email = input_email.getEditText().getText().toString();
+                password = input_password.getEditText().getText().toString();
+                if(email == null) {
+                    Toast.makeText(LoginActivity.this, "Please enter email!", Toast.LENGTH_SHORT).show();
+                } else if (password == null) {
+                    Toast.makeText(LoginActivity.this, "Please enter password!", Toast.LENGTH_SHORT).show();
+                } else {
+                    signIn(email, password);
+                }
+            }
+        });
+
+    }
+
+    private void signIn(final String email, final String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, AdminLandingActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Log.d("EMAIL", email);
+                            Log.d("PASSWORD", password);
+                            Toast.makeText(LoginActivity.this, "Incorrect email or password!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(this, AdminLandingActivity.class);
+            startActivity(intent);
+        }
     }
 }

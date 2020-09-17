736db819464449628feef20de8829bb5951dbcc7
prashantsingh <prashantsingh7623@gmail.com>
2020-09-17 21:58:50 +0530

    added curved bottom nav

--- app/src/main/java/adbudh/spit/admin/AdminLandingActivity.java
+++ app/src/main/java/adbudh/spit/admin/AdminLandingActivity.java
@@ -3,6 +3,7 @@ package adbudh.spit.admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import adbudh.spit.R;
 
@@ -13,4 +14,13 @@ public class AdminLandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_landing);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
